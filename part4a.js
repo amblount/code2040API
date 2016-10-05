4ad04cf198745548e2aa816e433caea7d6f72e0d
Alivia Blount <aliviablount@gmail.com>
2016-10-05 14:43:17 -0700

    tues

--- part4a.js
+++ part4a.js
@@ -1,71 +1,62 @@
 // Alivia Blount
-
 var request = require('request');
 var baseurl = "http://challenge.code2040.org/api/prefix";
 var token = "917cf8410513d1256d5c3024403ae85d";
 var nonPrefixStrings = [];
 var prefix, array, length;
 var answer;
-
 // get back a string from the success
 // need to parse it into json
 function success(err, res, body) {
   if (err) {
-    return console.error('upload failled:', err);
+    return console.error('upload failed:', err);
   }
-  console.log("res was", body);
+  secondReq.array = parseObj(body);
   secondReq.form.array = parseObj(body);
-  // console.log(secondReq.form.array)
-  console.log(res.headers);
-  request.post(secondReq, function(err, res, body){
-      console.log(secondReq);
-    if (err) {
-      return console.error('upload failed:', err);
-    }
-    // console.log("res 2 was", body);
-    console.log("second request was: ", secondReq);
-    console.log(res.body);
-  })
+  setTimeout(function() {
+    request.post(secondReq, function(err, res, body){
+      if (err) {
+        return console.error('upload failed:', err);
+      }
+      console.log(secondReq.array);
+      console.log(secondReq.form.array);
+    });
+  }, 1000);
 }
 
+function secondRequestCall(err, res, body) {
+  if (err) {
+    console.log('err with second req call:', err);
+    return err;
+  }
+}
 // method to parse object
 function parseObj(obj){
-  obj = JSON.parse(obj)
+  obj = JSON.parse(obj);
   prefix = obj["prefix"];
   array = obj["array"];
   length = prefix.length;
   nonPrefixStrings = array.filter(function(word) {
     return prefix !== word.substring(0, length);
   });
-  // console.log(nonPrefixStrings)
-  // console.log(Array.isArray(nonPrefixStrings));
-
   answer = nonPrefixStrings;
   return nonPrefixStrings;
-};
-
+}
 var firstReq = {
   url: baseurl,
+  token: token,
   form: {
     token: token
   }
-}
-
+};
 var secondReq = {
-  headers: {
-    // type: 'POST',
-    // token: token
-    // 'content-type': 'x-www-form-urlencoded'
-  },
   url: baseurl + "/validate",
+  // array: answer,
+  token: token,
   array: answer,
-  // token: token,
   form: {
     token: token,
     array: answer
   }
-}
-
-request.post(firstReq).pipe(request.post(secondReq));/* success)*/
-console.log("firstrequest was: ", firstReq);
-console.log("=================================================")
+};
+request.post(firstReq, success);
